98e5a018d41a6689e383c380489a44112f782ce9
Nikola Derikonjic <nikola@pregmatch.org>
2022-06-28 12:39:59 +0200

    updaate

--- src/BaGet.Web/BaGetEndpointBuilder.cs
+++ src/BaGet.Web/BaGetEndpointBuilder.cs
@@ -23,7 +23,7 @@ namespace BaGet
         {
             endpoints.MapControllerRoute(
                 name: Routes.IndexRouteName,
-                pattern: "v3/index.json",
+                pattern: "nuget/v3/index.json",
                 defaults: new { controller = "ServiceIndex", action = "Get" });
         }
 
@@ -31,19 +31,19 @@ namespace BaGet
         {
             endpoints.MapControllerRoute(
                 name: Routes.UploadPackageRouteName,
-                pattern: "api/v2/package",
+                pattern: "nuget/api/v2/package",
                 defaults: new { controller = "PackagePublish", action = "Upload" },
                 constraints: new { httpMethod = new HttpMethodRouteConstraint("PUT") });
 
             endpoints.MapControllerRoute(
                 name: Routes.DeleteRouteName,
-                pattern: "api/v2/package/{id}/{version}",
+                pattern: "nuget/api/v2/package/{id}/{version}",
                 defaults: new { controller = "PackagePublish", action = "Delete" },
                 constraints: new { httpMethod = new HttpMethodRouteConstraint("DELETE") });
 
             endpoints.MapControllerRoute(
                 name: Routes.RelistRouteName,
-                pattern: "api/v2/package/{id}/{version}",
+                pattern: "nuget/api/v2/package/{id}/{version}",
                 defaults: new { controller = "PackagePublish", action = "Relist" },
                 constraints: new { httpMethod = new HttpMethodRouteConstraint("POST") });
         }
@@ -52,18 +52,18 @@ namespace BaGet
         {
             endpoints.MapControllerRoute(
                 name: Routes.UploadSymbolRouteName,
-                pattern: "api/v2/symbol",
+                pattern: "nuget/api/v2/symbol",
                 defaults: new { controller = "Symbol", action = "Upload" },
                 constraints: new { httpMethod = new HttpMethodRouteConstraint("PUT") });
 
             endpoints.MapControllerRoute(
                 name: Routes.SymbolDownloadRouteName,
-                pattern: "api/download/symbols/{file}/{key}/{file2}",
+                pattern: "nuget/api/download/symbols/{file}/{key}/{file2}",
                 defaults: new { controller = "Symbol", action = "Get" });
 
             endpoints.MapControllerRoute(
                 name: Routes.PrefixedSymbolDownloadRouteName,
-                pattern: "api/download/symbols/{prefix}/{file}/{key}/{file2}",
+                pattern: "nuget/api/download/symbols/{prefix}/{file}/{key}/{file2}",
                 defaults: new { controller = "Symbol", action = "Get" });
         }
 
@@ -71,18 +71,18 @@ namespace BaGet
         {
             endpoints.MapControllerRoute(
                 name: Routes.SearchRouteName,
-                pattern: "v3/search",
+                pattern: "nuget/v3/search",
                 defaults: new { controller = "Search", action = "Search" });
 
             endpoints.MapControllerRoute(
                 name: Routes.AutocompleteRouteName,
-                pattern: "v3/autocomplete",
+                pattern: "nuget/v3/autocomplete",
                 defaults: new { controller = "Search", action = "Autocomplete" });
 
             // This is an unofficial API to find packages that depend on a given package.
             endpoints.MapControllerRoute(
                 name: Routes.DependentsRouteName,
-                pattern: "v3/dependents",
+                pattern: "nuget/v3/dependents",
                 defaults: new { controller = "Search", action = "Dependents" });
         }
 
@@ -90,12 +90,12 @@ namespace BaGet
         {
             endpoints.MapControllerRoute(
                name: Routes.RegistrationIndexRouteName,
-               pattern: "v3/registration/{id}/index.json",
+               pattern: "nuget/v3/registration/{id}/index.json",
                defaults: new { controller = "PackageMetadata", action = "RegistrationIndex" });
 
             endpoints.MapControllerRoute(
                 name: Routes.RegistrationLeafRouteName,
-                pattern: "v3/registration/{id}/{version}.json",
+                pattern: "nuget/v3/registration/{id}/{version}.json",
                 defaults: new { controller = "PackageMetadata", action = "RegistrationLeaf" });
         }
 
@@ -103,27 +103,27 @@ namespace BaGet
         {
             endpoints.MapControllerRoute(
                 name: Routes.PackageVersionsRouteName,
-                pattern: "v3/package/{id}/index.json",
+                pattern: "nuget/v3/package/{id}/index.json",
                 defaults: new { controller = "PackageContent", action = "GetPackageVersions" });
 
             endpoints.MapControllerRoute(
                 name: Routes.PackageDownloadRouteName,
-                pattern: "v3/package/{id}/{version}/{idVersion}.nupkg",
+                pattern: "nuget/v3/package/{id}/{version}/{idVersion}.nupkg",
                 defaults: new { controller = "PackageContent", action = "DownloadPackage" });
 
             endpoints.MapControllerRoute(
                 name: Routes.PackageDownloadManifestRouteName,
-                pattern: "v3/package/{id}/{version}/{id2}.nuspec",
+                pattern: "nuget/v3/package/{id}/{version}/{id2}.nuspec",
                 defaults: new { controller = "PackageContent", action = "DownloadNuspec" });
 
             endpoints.MapControllerRoute(
                 name: Routes.PackageDownloadReadmeRouteName,
-                pattern: "v3/package/{id}/{version}/readme",
+                pattern: "nuget/v3/package/{id}/{version}/readme",
                 defaults: new { controller = "PackageContent", action = "DownloadReadme" });
 
             endpoints.MapControllerRoute(
                 name: Routes.PackageDownloadIconRouteName,
-                pattern: "v3/package/{id}/{version}/icon",
+                pattern: "nuget/v3/package/{id}/{version}/icon",
                 defaults: new { controller = "PackageContent", action = "DownloadIcon" });
         }
     }
